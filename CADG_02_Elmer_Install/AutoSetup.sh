a16d0294e62c4938f713768e8c915163f9f60530
dymaxionkim <dymaxion.kim@gmail.com>
2017-08-07 20:46:40 -0400

    AutoSetup.sh V01

--- CADG_02_Elmer_Install/AutoSetup.sh
+++ CADG_02_Elmer_Install/AutoSetup.sh
@@ -5,26 +5,6 @@
 # by Dymaxion.kim@gmail.com
 ###########################################################
 
-# Howto
-# 0. 설치 요구 조건
-#    가급적 속도가 좋은 인터넷에 연결되어 있을 것.
-#    PC의 CPU는 가급적 4스레드 이상 지원할 것.
-#    PC의 메모리는 8GB 이상일 것.
-# 1. 윈도OS PC에서 엔지니어링 연습용 리눅스 가상머신을 설치하는 것이 목표
-# 2. 아래 주소로 가서 최신버전의 'Windows hosts' 바이너리설치파일을 다운로드 받아, 실행시켜 설치한다.
-#    https://www.virtualbox.org/wiki/Downloads
-# 3. 아래 주소로 가서 `Lubuntu 16.04.2 Xenial Xerus VirtualBox (VDI) 64bit` 파일을 다운로드 받는다.
-#    http://www.osboxes.org/lubuntu/
-# 4. 버츄얼박스를 실행시키고, '새로만들기' 버튼을 누른 후, 다운로드 받은 가상머신 파일을 연결해서 가상머신을 셋팅한다.
-#    CPU 코어는 가급적 2개 이상 할당해 주고, RAM은 가급적 4096MB 이상 할당해 준다.
-#    네트워크는 '브리지'로 선택해 준다.
-# 5. 구축된 가상머신을 시작한다.  가상머신이 부팅되면 로그인 비밀번호는 'osboxes.org'를 쳐 준다.
-# 6. 터미널을 열고 현재 홈디렉토리 위치에 있는지 확인한다.
-# 7. 본 스크립트 파일을 다운로드 받는다.
-# 8. 'chmod +x ./AutoSetup.sh' 명령을 쳐 넣어 실행 가능하도록 한다.
-# 9. 버츄얼박스 메뉴의 '장치'에서 '게스트 확장 CD 이미지 삽입'을 선택해 준다.
-# 10. 'sudo sh AutoSetup.sh' 명령을 쳐 넣어 스크립트를 실행한다.
-# 11. 다 되면 가상머신을 재부팅한다.
 
 
 # Repositories
@@ -49,8 +29,7 @@ apt -y dist-upgrade
 
 # VirtualBox
 apt -y install build-essential linux-headers-`uname -r`
-cd /media/osboxes/VBOXADDITIONS*
-sh ./VBoxLinuxAdditions.run
+sh /media/osboxes/VBOXADDITIONS*/VBoxLinuxAdditions.run
 usermod -G vboxsf -a osboxes
 
 
@@ -90,121 +69,7 @@ apt -y install atom
 apt -y install freecad netgen
 
 mv /usr/share/netgen/drawing.tcl /usr/share/netgen/drawing.tcl.bak
-echo '# -*- mode: TCL; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-' > /usr/share/netgen/drawing.tcl
-echo '#' >> /usr/share/netgen/drawing.tcl
-echo '# drawing.tcl --' >> /usr/share/netgen/drawing.tcl
-echo '#' >> /usr/share/netgen/drawing.tcl
-echo '#' >> /usr/share/netgen/drawing.tcl
-echo '#  Creates a drawing frame, and binds mouse events' >> /usr/share/netgen/drawing.tcl
-echo '#' >> /usr/share/netgen/drawing.tcl
-echo 'set oldmousex 0' >> /usr/share/netgen/drawing.tcl
-echo 'set oldmousey 0' >> /usr/share/netgen/drawing.tcl
-echo '#' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '# if { 1 } {' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '# use this one for Togl 2.0' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '# if {[catch {togl .ndraw -width 400 -height 300  -rgba true -double true -depth true -privatecmap false -stereo false -indirect true -create init  -display draw -reshape reshape  }] } {' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '# changed -indirect true/false !!!' >> /usr/share/netgen/drawing.tcl
-echo 'if {[catch {togl .ndraw -width 400 -height 300  -rgba true -double true -depth true -privatecmap false -stereo false -indirect false }] } {    ' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '    puts "no OpenGL" ' >> /usr/share/netgen/drawing.tcl
-echo '} {' >> /usr/share/netgen/drawing.tcl
-echo '    #' >> /usr/share/netgen/drawing.tcl
-echo '    pack .ndraw -expand true -fill both -padx 10 -pady 10' >> /usr/share/netgen/drawing.tcl
-echo '    #' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <Button-1> {' >> /usr/share/netgen/drawing.tcl
-echo '  set oldmousex %x; set oldmousey %y;' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <Button-2> {' >> /usr/share/netgen/drawing.tcl
-echo '  set oldmousex %x; set oldmousey %y;' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <Button-3> {' >> /usr/share/netgen/drawing.tcl
-echo '  set oldmousex %x; set oldmousey %y;' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <B1-Motion> {' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_MouseMove $oldmousex $oldmousey %x %y $drawmode' >> /usr/share/netgen/drawing.tcl
-echo '  .ndraw render' >> /usr/share/netgen/drawing.tcl
-echo '  set oldmousex %x; set oldmousey %y;' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <Double-1> {' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_MouseDblClick %x %y' >> /usr/share/netgen/drawing.tcl
-echo '  .ndraw render' >> /usr/share/netgen/drawing.tcl
-echo '  if { [winfo exists .bcprop_dlg] } { bcpropdialog }' >> /usr/share/netgen/drawing.tcl
-echo '  if { [winfo exists .surfacemeshsize_dlg] } { surfacemeshsizedialog }' >> /usr/share/netgen/drawing.tcl
-echo '  if { [winfo exists .fieldlines_dlg] } { fieldlinesdialog }' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <B2-Motion> {' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_MouseMove $oldmousex $oldmousey %x %y move' >> /usr/share/netgen/drawing.tcl
-echo '  .ndraw render' >> /usr/share/netgen/drawing.tcl
-echo '  set oldmousex %x; set oldmousey %y;' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '    bind .ndraw <B3-Motion> {' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_MouseMove $oldmousex $oldmousey %x %y zoom' >> /usr/share/netgen/drawing.tcl
-echo '  .ndraw render' >> /usr/share/netgen/drawing.tcl
-echo '  set oldmousex %x; set oldmousey %y;' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '}' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo 'proc popupcheckredraw { vari { x 0 } } {' >> /usr/share/netgen/drawing.tcl
-echo '    upvar $vari varname' >> /usr/share/netgen/drawing.tcl
-echo '    if { $varname == 1 } {' >> /usr/share/netgen/drawing.tcl
-echo '  set varname 0' >> /usr/share/netgen/drawing.tcl
-echo '    } {' >> /usr/share/netgen/drawing.tcl
-echo '        #        puts "popup-redraw $vari"' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_Vis_Set parameters' >> /usr/share/netgen/drawing.tcl
-echo '  redraw' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '}' >> /usr/share/netgen/drawing.tcl
-echo 'proc popupcheckredraw2 { vari boolvar { x 0 } } {' >> /usr/share/netgen/drawing.tcl
-echo '    upvar $vari varname' >> /usr/share/netgen/drawing.tcl
-echo '    if { $varname == 1 } {' >> /usr/share/netgen/drawing.tcl
-echo '  set varname 0' >> /usr/share/netgen/drawing.tcl
-echo '    } {' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_SetVisParameters' >> /usr/share/netgen/drawing.tcl
-echo '  if { $boolvar == 1 } { redraw }' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_SetVisParameters' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '}' >> /usr/share/netgen/drawing.tcl
-echo 'proc popupcheckredraw3 { vari { x 0 } } {' >> /usr/share/netgen/drawing.tcl
-echo '    upvar $vari varname' >> /usr/share/netgen/drawing.tcl
-echo '    if { $varname == 1 } {' >> /usr/share/netgen/drawing.tcl
-echo '  set varname 0' >> /usr/share/netgen/drawing.tcl
-echo '    } {' >> /usr/share/netgen/drawing.tcl
-echo '  Ng_Vis_Set parameters' >> /usr/share/netgen/drawing.tcl
-echo '    }' >> /usr/share/netgen/drawing.tcl
-echo '}' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo 'proc redraw { {x 0} } {' >> /usr/share/netgen/drawing.tcl
-echo '    if {[winfo exists .ndraw]} { .ndraw render } ' >> /usr/share/netgen/drawing.tcl
-echo '}' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Left> { Ng_MouseMove 0 0 -10 0 rotate; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Right> { Ng_MouseMove 0 0 10 0 rotate; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Up> { Ng_MouseMove 0 0 0 -10 rotate; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Down> { Ng_MouseMove 0 0 0 10 rotate; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Shift-Left> { Ng_MouseMove 0 0 -10 0 move; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Shift-Right> { Ng_MouseMove 0 0 10 0 move; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Shift-Up> { Ng_MouseMove 0 0 0 -10 move; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Shift-Down> { Ng_MouseMove 0 0 0 10 move; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Control-Up> { Ng_MouseMove 0 0 0 -10 zoom; redraw }' >> /usr/share/netgen/drawing.tcl
-echo 'bind . <Control-Down> { Ng_MouseMove 0 0 0 10 zoom; redraw }' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo 'bind all <Button-4> \' >> /usr/share/netgen/drawing.tcl
-echo '   {event generate [focus -displayof %W] <MouseWheel> -delta  120}' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo ' bind all <Button-5> \' >> /usr/share/netgen/drawing.tcl
-echo '   {event generate [focus -displayof %W] <MouseWheel> -delta -120}' >> /usr/share/netgen/drawing.tcl
-echo '' >> /usr/share/netgen/drawing.tcl
-echo 'bind all <MouseWheel> { Ng_MouseMove 0 0 0 [expr {%D/-5}] zoom; redraw }' >> /usr/share/netgen/drawing.tcl
+cp ./drawing.tcl /usr/share/netgen/drawing.tcl
 
 
 # Nimf
@@ -224,18 +89,13 @@ pip install pandoc
 
 # GNU Octave
 apt -y install octave liboctave-dev
-chown -hR osboxes:osboxes ~/.config/octave
-
-
-# Locale
-echo 'LANG="ko_KR.UTF-8"' > /etc/default/locale
+chown -hR osboxes:osboxes /home/osboxes/.config/octave
 
 
 # Elmer
 apt -y install elmerfem-csc
 cp /usr/share/ElmerGUI/edf-extra/*.xml /usr/share/ElmerGUI/edf/
 
-wget -O /usr/share/applications/ElmerGUI.desktop "https://raw.githubusercontent.com/dankamongmen/sprezzos-world/master/packaging/elmerfem/debian/ElmerGUI.desktop"
 mkdir /usr/share/ElmerGUI/icons
 wget -O /usr/share/ElmerGUI/icons/Mesh3D.png "https://raw.githubusercontent.com/tehnick/elmerfem/master/ElmerGUI/Application/icons/Mesh3D.png"
 
@@ -270,7 +130,7 @@ echo 'Categories=Education;' >> /usr/share/applications/ElmerGUI.desktop
 # Gmsh
 wget -O Gmsh.tgz "http://gmsh.info/bin/Linux/gmsh-3.0.4-Linux64.tgz"
 tar -xvzf Gmsh.tgz
-mv gmsh* Gmsh
+mv ./gmsh* /home/osboxes/Gmsh
 rm Gmsh.tgz
 
 echo '' >> ~/.bashrc
@@ -292,7 +152,7 @@ echo 'Categories=Education;' >> /usr/share/applications/Gmsh.desktop
 # Paraview
 wget -O Paraview.tar.gz "https://www.paraview.org/paraview-downloads/download.php?submit=Download&version=v5.4&type=binary&os=linux64&downloadFile=ParaView-5.4.1-RC3-Qt5-OpenGL2-MPI-Linux-64bit.tar.gz"
 tar -xvzf Paraview.tar.gz
-mv ParaView-* Paraview
+mv ./ParaView-* /home/osboxes/Paraview
 rm Paraview.tar.gz
 
 echo '' >> ~/.bashrc
@@ -314,7 +174,7 @@ echo 'Categories=Education;' >> /usr/share/applications/Paraview.desktop
 # Salome
 wget -O Salome.tgz "http://www.salome-platform.org/downloads/current-version/DownloadDistr?platform=UB16.04&version=8.2.0"
 tar -xvzf Salome.tgz
-mv SALOME-* Salome
+mv ./SALOME-* /home/osboxes/Salome
 rm Salome.tgz
 
 echo '' >> ~/.bashrc
@@ -356,6 +216,9 @@ cp /lubuntu-rc.xml /home/osboxes/.config/openbox/lubuntu-rc.xml
 # pcmanfm
 mv /home/osboxes/.config/pcmanfm/lubuntu/desktop-items-0.conf /home/osboxes/.config/pcmanfm/lubuntu/desktop-items-0.conf.bak
 cp ./desktop-items-0.conf /home/osboxes/.config/pcmanfm/lubuntu/desktop-items-0.conf
+
+# Locale
+echo 'LANG="ko_KR.UTF-8"' > /etc/default/locale
 
 
 # Fin
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Elmer_Examples_for_CADG
-Elmer FEM Examples for CAD&amp;Graphics
+
+__Elmer FEM Examples for CAD&Graphics__
+
+
+## 1. 개요(Intro)
+
+* 2017년도 CAD&Graphics 연재 기고문에 필요한 보조 자료 모음.
+* "엘머로 해 보는 오픈소스 엔지니어링"
+
+
+## 2. 설치(Installation)
+
+### (1) 개요
+* 윈도OS PC에서 엔지니어링 연습용 리눅스 가상머신을 설치하는 것이 목표
+* 쉘스크립트로 필요한 소프트웨어와 환경을 자동으로 구성하도록 하여 최대한 신속하게 완료하도록 하려는 의도
+
+### (2) 요구조건(Pre-Requisites)
+* 가급적 속도가 좋은 인터넷에 연결되어 있을 것. (많은 다운로드가 있기 때문)
+* PC의 CPU는 가급적 4스레드 이상 지원할 것. (계산작업시 멀티코어 연산이 유리함)
+* PC의 메모리는 8GB 이상일 것. (가상머신에 최소한 4GB 이상 할당하는 것이 좋음)
+
+### (3) 버츄얼박스(VirtualBox) 및 루분투(Lubuntu)설치
+* [버츄얼박스 홈페이지](https://www.virtualbox.org/wiki/Downloads)로 가서 최신버전의 'Windows hosts' 바이너리설치파일을 다운로드 받아, 실행시켜 설치한다.
+* [osboxes 홈페이지](http://www.osboxes.org/lubuntu/)로 가서 `Lubuntu 16.04.2 Xenial Xerus VirtualBox (VDI) 64bit` 파일을 다운로드 받는다(17.04 이후 버전은 안됨. 32비트 버전도 안됨.).
+* 버츄얼박스를 실행시키고, '새로만들기' 버튼을 누른 후, 다운로드 받은 가상머신 파일을 연결해서 가상머신을 셋팅한다.
+* 이때 가상머신의 CPU 코어는 가급적 2개 이상 할당해 주고, RAM은 가급적 4096MB 이상 할당해 준다.  네트워크는 '브리지'로 선택해 준다.
+* 구축된 가상머신을 시작한다.  가상머신이 부팅되면 로그인 비밀번호는 'osboxes.org'를 쳐 준다(로그인한 후 편한걸로 바꾸면 된다).
+
+### (4) Github에 있는 참고자료 받기
+* `Ctrl+Alt+t` 단축키를 눌러 터미널을 열고 다음 명령을 수행한다.
+
+```bash
+mkdir /home/osboxes/github
+cd /home/osboxes/github
+git clone https://github.com/dymaxionkim/Elmer_Examples_for_CADG.git
+```
+
+### (5) 자동 어플리케이션 설치 및 환경설정 작업 수행
+* 먼저 버츄얼박스 메뉴의 '장치'에서 '게스트 확장 CD 이미지 삽입'을 선택해 준다. (버츄얼박스 게스트에디션을 설치할 수 있도록 가상CD를 삽입하는 것)
+* 터미널에서 다음 명령을 계속 이어서 수행한다.
+
+```bash
+cd /home/osboxes/github/Elmer_Examples_for_CADG/CADG_02_Elmer_Install
+chmod +x ./AutoSetup.sh
+sudo sh AutoSetup.sh
+```
+
+* 이제 다 될 때 까지 한참 기다린다.
+* 운좋게 이상없이 완료되면, 가상머신을 재부팅한다.
+
+
+
+
